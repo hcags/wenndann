c1ee398f7bbc3e220e8682e1148e313a8bd8f5b5
hcags <120849679+hcags@users.noreply.github.com>
2023-10-24 15:56:15 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Run = !(Run)
 })
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showString("Hast du mich grade umgetret ")
+    basic.setLedColor(basic.rgb(255, 0, 0))
+    basic.showString("Drest du mich bitte richtigrum ")
+})
 let Run = false
 let Count = 0
 Run = true
