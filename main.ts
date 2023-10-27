e4f5da9be542c068724fdae94f1a8a7c11bc2a58
hcags <120849679+hcags@users.noreply.github.com>
2023-10-27 19:38:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Run = !(Run)
 })
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    if (Count) {
+        Count = 70
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (Count) {
+        Count = 30
+    }
+})
 input.onGesture(Gesture.LogoDown, function () {
     basic.showString("Hast du mich grade umgetret ")
     basic.setLedColor(basic.rgb(255, 0, 0))
@@ -8,12 +18,13 @@ input.onGesture(Gesture.LogoDown, function () {
 })
 let Run = false
 let Count = 0
+Count = 0
 Run = true
 basic.forever(function () {
     if (Run) {
         Count += 1
         basic.showString("" + (Count))
-        if (10 == Count) {
+        if (100 == Count) {
             Count = 0
         }
     }
