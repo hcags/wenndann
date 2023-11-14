dc969106ac33d4b92f88388e87b4a2f88b59f620
hcags <120849679+hcags@users.noreply.github.com>
2023-11-14 06:05:12 -0800

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,25 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Direction2 = 0
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     Run = !(Run)
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Direction2 = -1
+})
+let Direction2 = 0
 let Run = false
 let Count = 0
 Run = true
+Direction2 += 1
 basic.forever(function () {
     if (Run) {
-        Count += 1
+        Count += Direction2
         basic.showString("" + (Count))
         if (10 == Count) {
             Count = 0
+        } else if (0 == Count) {
+            Direction2 = 10
         }
     }
 })
