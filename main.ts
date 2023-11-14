df336922540feb86c814ce6bbf62f6a19e58803f
hcags <120849679+hcags@users.noreply.github.com>
2023-11-14 05:10:36 -0800

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,51 +1,14 @@
-input.onGesture(Gesture.ScreenDown, function () {
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     Run = !(Run)
 })
-input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    if (Count) {
-        Count = 70
-    }
-})
-input.onGesture(Gesture.Shake, function () {
-    if (Count) {
-        Count = 30
-    }
-})
-input.onGesture(Gesture.LogoDown, function () {
-    basic.showString("Hast du mich grade umgetret ")
-    basic.setLedColor(basic.rgb(255, 0, 0))
-    basic.showString("Drest du mich bitte richtigrum ")
-})
-input.onGesture(Gesture.ScreenUp, function () {
-    if (Count) {
-        Count = 10
-    }
-})
 let Run = false
 let Count = 0
-Count = 0
 Run = true
 basic.forever(function () {
     if (Run) {
         Count += 1
         basic.showString("" + (Count))
-        if (100 == Count) {
+        if (10 == Count) {
             Count = 0
         }
     }
