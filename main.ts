4431cfa6ce2e0a3054619308e1db7613ee89073e
hcags <120849679+hcags@users.noreply.github.com>
2023-11-14 06:05:03 -0800

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,25 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Direction2 = -1
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     Run = !(Run)
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Direction2 = 1
+})
+let Direction2 = 0
 let Run = false
 let Count = 0
 Run = true
+Direction2 = 1
 basic.forever(function () {
     if (Run) {
-        Count += 1
+        Count += Direction2
         basic.showString("" + (Count))
-        if (10 == Count) {
+        if (Count == 10 && Direction2 == 1) {
             Count = 0
+        } else if (Count == 0 && Direction2 == -1) {
+            Count = 10
         }
     }
 })
